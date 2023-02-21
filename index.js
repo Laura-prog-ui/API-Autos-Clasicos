06144c7753f4a5c53cc4434e72300887f76cbdba
Laura-prog-ui <duarteriveralauraestefania0@gmail.com>
2023-02-21 17:51:44 -0500

    Segundo commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hola Mundo")
+const express = require('express');
+const  mongoose  = require('mongoose');
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 3030
+
+//routes
+app.get("/", (req, res) => {
+    res.send("Welcome to my API");
+});
+
+//mongodb connection
+mongoose.set('strictQuery', true);
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then (() => console.log("Connected to MongoDB"))
+    .catch ((error) => console.error(error));
+
+app.listen(port, () => console.log('server listening on port', 3030));

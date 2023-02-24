005bb31272bf3a2da3f5ffa8d1f27fd1e84f5c57
Laura-prog-ui <duarteriveralauraestefania0@gmail.com>
2023-02-23 23:35:35 -0500

    middleware

--- app/controlles/autos.js
+++ app/controlles/autos.js
@@ -1,19 +1,31 @@
-// const { httpError } = require('../helpers/handleError')
-// const userModel = require('../models/autos')
+const { httpError } = require('../helpers/handleError')
+const userModel = require('../models/autos')
 
-const getItems = (req, res) => {
-    res.send({list:[1,2,3]})
-
-
+const getItems = async (req, res) => {
+    try{
+        const listAll = await userModel.find({})
+        res.send({ data: listAll })
+    } catch (e) {
+        httpError(res, e)
+    }
 }
 
 const getItem = () => {
 
 }
 
-const createItem = () => {
+    const createItem = async (req, res) => {
+        try {
+            const { image_url, brand, age, model, country } = req.body
+            const resDetail = await userModel.create({
+                image_url, brand, age, model, country
+            })
+            res.send({ data: resDetail })
+        } catch (e) {
+            httpError(res, e)
+        }
+    }
 
-}
 
 const updateItem = () => {
 
--- app/routes/autos.js
+++ app/routes/autos.js
@@ -2,11 +2,11 @@ const express = require("express")
 const router = express.Router()
 const { getItems, getItem, createItem, deleteItem, updateItem } = require('../controlles/autos')
 
-router.get('/', getItems)
+router.get('/', checkOrigin,  getItems)
 
-router.get('/:id', getItem)
+router.get('/:id', checkOrigin,getItem)
 
-router.post('/', createItem)
+router.post('/', checkOrigin,createItem)
 
 router.patch('/:id', updateItem)
 

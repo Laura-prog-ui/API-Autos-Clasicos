9ee2e4b5782365e77e2593672f9331edaa2c9de0
Laura-prog-ui <duarteriveralauraestefania0@gmail.com>
2023-03-21 16:18:15 -0500

    Swagger

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
-require('dotenv').config()//variables de entorno
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const PORT = process.env.PORT || 3030 //puerto declarado en la variable de entorno
-const { dbConnect } = require('./config/mongo') //requiere la conexion
+const path = require("path")
+const { dbConnect } = require('./config/mongo')
+const PORT = process.env.PORT || 3030
+
+//swagger
+const swaggerUI = require('swagger-ui-express')
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerSpec ={ //inicio de swagger
+  definition:{
+    openapi: "3.0.0",
+    info:{
+      title:"autos API",
+      version: "1.0"
+    },
+    servers:[
+      {
+        url: "https://apiautos.onrender.com/api/1.0/autos"
+      }
+    ]
+  },
+  apis:[`${path.join(__dirname, "./app/routes/autos.js")}`] //rutas
+}
 
 app.use(cors())
-app.use(express.json()) //permite envie de datos
-
+app.use(express.json())
 app.use('/api/1.0', require('./app/routes'))
+app.use('/api-doc', swaggerUI.serve , swaggerUI.setup(swaggerJsDoc(swaggerSpec))) //ruta de la documentacion
 
 dbConnect()
-app.listen(PORT, ()=>{ //Api escuchando
-  console.log('api lista por el puerto', PORT)
+app.listen(PORT, () => {
+    console.log('API lista por el puerto ', PORT)
 })
--- app/routes/autos.js
+++ app/routes/autos.js
@@ -1,15 +1,142 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router()
-const { getItems, getItem, createItem, deleteItem, updateItem } = require('../controllers/autos')
+const { getItems, getItem, createItem, deleteItem, updateItem } = require('../controllers/Humedales')
 
-router.get('/',  getItems)
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    autosapi:
+ *      type: object
+ *      properties:
+ *         img_url:
+ *           type: string
+ *           description: img
+ *         brand:
+ *           type: string
+ *           description: brand name
+ *         age:
+ *           type: number
+ *           description: year of production
+ *         model:
+ *           type: string
+ *           description: model of each car
+ *         country:
+ *           type: string
+ *           description: Country where the brand was founded
+ *         description:
+ *           type: string
+ *           description: overview of each car
+ */
+
+/**
+ * @swagger
+ * /:
+ *  get:
+ *    sumary: return all autos
+ *    tags: [autosapi]
+ *    responses:
+ *      200:
+ *        description: all autos!
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                 $ref: '#/components/schemas/autosapi'
+ */
+router.get('/' , getItems)
+/**
+ * @swagger
+ * /{id}:
+ *  get:
+ *    sumary: return one autos
+ *    tags: autosapi]
+ *    parameters:
+ *    - in: path
+ *      name: id
+ *      schema:
+ *        type: string
+ *        required: true
+ *        description: the user id
+ *    responses:
+ *      200:
+ *        description: one create autos!
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                 $ref: '#/components/schemas/autosapi'
+ *      400:
+ *       description: user not found!
+ */
 router.get('/:id', getItem)
 
+/**
+ * @swagger
+ * /:
+ *  post:
+ *    sumary: create a new auto
+ *    tags: [autosapi]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *         schema:
+ *          type: object
+ *          $ref: '#/components/schemas/autosapi'
+ *    responses:
+ *      200:
+ *        description: nuevo auto creado!
+ */
 router.post('/', createItem)
 
-router.patch('/:id', updateItem)
 
+
+/**
+ * @swagger
+ * /{id}:
+ *  patch:
+ *    sumary: update one pays
+ *    tags: [autosapi]
+ *    parameters:
+ *    - in: path
+ *      name: id
+ *      schema:
+ *        type: string
+ *        required: true
+ *        description: the user id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *         schema:
+ *          type: object
+ *          $ref: '#/components/schemas/autosapi'
+ *    responses:
+ *      200:
+ *        description: pay update!
+ */
+router.patch('/:id', updateItem)
+/**
+ * @swagger
+ * /{id}:
+ *  delete:
+ *    sumary: return one autos
+ *    tags: [autosapi]
+ *    parameters:
+ *    - in: path
+ *      name: id
+ *      schema:
+ *        type: string
+ *        required: true
+ *        description: the user id
+ *    responses:
+ *      200:
+ *        description: pay update!
+ */
 router.delete('/:id', deleteItem)
 
 module.exports = router
